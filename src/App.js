dbb34a106eec363f277c4710e73ffc325271567c
dominikawalska <dominikawalska@gmail.com>
2022-05-16 22:09:52 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function App() {
+  return (
+    <Container>
+      <header className="App-header">
+        <h1>📙 Dictionary</h1>
+      </header>
+    </Container>
+  );
+}
