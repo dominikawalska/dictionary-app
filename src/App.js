44bf55fcbd365efd011f8adfba62a8786950b265
dominikawalska <dominikawalska@gmail.com>
2022-05-22 20:08:11 +0200

    Rendered meanings and definitions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.css";
+import Dictionary from "./Dictionary";
 import Container from "react-bootstrap/Container";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 export default function Dictionary() {
-  let [keyword, setKeyword] = useState(null);
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -34,6 +36,7 @@ export default function Dictionary() {
         ></input>
         <Button type="Submit">Submit</Button>
       </Form>
+      <Results results={results} />
     </div>
   );
 }
